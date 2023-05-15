97ef934dd18f3307de3b440d98c2133d107bcb79
yachen <yachen0@nuvoton.com>
2023-05-15 11:56:03 +0800

    1. Re-commit 2. Add dts/nuvoton/numicro/M46x-pinctrl.h 3. Add m48x.

---
+++ dts/nuvoton/numicro/M46x-pinctrl.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2023 Nuvoton
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/dt-bindings/pinctrl/numicro-pinctrl.h>
+
+
+#define UART0_RXD_PB12 NUMICRO_PINMUX('B', 12, 6)
+#define UART0_TXD_PB13 NUMICRO_PINMUX('B', 13, 6)
